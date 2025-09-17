4b2cc5be2bd20581e636fca8e93f81457b76504e
Sahil Chaudhari <sahilrchaudhari0607@gmail.com>
2025-09-17 09:46:11 +0530

    Assignment 1

---
+++ Tude assignment 1 task 2.py
@@ -0,0 +1,8 @@
+
+print('Enter your first name')
+a=input()
+print(a)
+print('Enter your last name')
+b=input()
+
+print('Hello '+ a+' '+b +' Welcome to Python program.')
---
+++ Tutedude assignment 1 task 1.py
@@ -0,0 +1,14 @@
+
+print('Input a number')
+a = int(input())
+print('Input a number')
+b = int(input())
+
+print('Additon of the given numbers is')
+print(a + b)
+print('Subtraction of the given numbers is')
+print(a - b)
+print('Multiplication of the given numbers is')
+print(a * b)
+print('Division of the given numbers is')
+print(a / b)
